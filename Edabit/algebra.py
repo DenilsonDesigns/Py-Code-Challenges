da2b796957ca9953bdc057b30f2a726fa3adc2d6
Languiller <Daniel.Languiller@team.telstra.com>
2020-12-15 15:46:16 +1100

    WIP

--- Edabit/algebra.py
+++ Edabit/algebra.py
@@ -1,3 +1,6 @@
+import math
+
+
 def calculator(txt):
     return eval(txt)
 
@@ -35,6 +38,26 @@ def is_automorphic(n):
     return int(repr(n ** 2)[-len(str(n)):]) == n
 
 
-print(is_automorphic(36))
-print(is_automorphic(5))
-print(is_automorphic(25))
+def count(deck):
+    return sum([1 if x in [2, 3, 4, 5, 6] else 0 if x in [7, 8, 9] else -1 for x in deck])
+
+
+def total_distance(height, length, tower):
+    return round(tower / height * (height + length), 1)
+
+
+def color_invert(rgb):
+    return tuple(255 - c for c in rgb)
+
+
+def get_distance(a, b):
+    return round(math.sqrt((a['x'] - b['x'])**2 + (a['y'] - b['y'])**2), 3)
+
+
+def adjacent_product(lst):
+    return max(a*b for a, b in zip(lst, lst[1:]))
+
+
+print(adjacent_product([3, 6, -2, -5, 7, 3]))
+print(adjacent_product([5, 6, -4, 2, 3, 2, -23]))
+print(adjacent_product([0, -1, 1, 24, 1, -4, 8, 10]))
